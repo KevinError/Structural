c2cd5f8909a87712aa60d8ef7d2212b7ab10b19c
Hyunwoo Kim <khk4084417@gmail.com>
2023-04-08 16:07:21 -0700

    Update DecoratorMain.java

--- src/DecoratorMain.java
+++ src/DecoratorMain.java
@@ -1,8 +1,24 @@
+import decorator.*;
+
 public class DecoratorMain {
 
     public static void main(String[] args){
+        Order firstOrder = new Order();
+        LoyaltyDiscount firstDiscount = new LoyaltyDiscount(199);
+        Order secondOrder = new Order();
+        LoyaltyDiscount secondDiscount = new LoyaltyDiscount(50);
+        Order thirdOrder = new Order();
+        LoyaltyDiscount thirdDiscount = new LoyaltyDiscount(20);
 
-        
+        FoodItem burger = new Burger(5.0);
+        FoodItem drink = new Drink(1.5);
+        FoodItem fries = new Fries(2);
+
+        firstOrder.addFoodItem(new Bacon(burger));
+        firstOrder.addFoodItem(drink);
+        firstOrder.addFoodItem(fries);
+        System.out.println(firstDiscount.applyDiscount(firstOrder.calculateTotalCost()));
+
 
     }
 }
